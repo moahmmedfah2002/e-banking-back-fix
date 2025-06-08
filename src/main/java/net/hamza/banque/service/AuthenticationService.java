2fe3f0ab5400b5f697ff146ec20c13f30b31f164
fahlaoui <fahlaouimohammed@gmail.com>
2025-06-08 14:08:55 +0100

    ci

--- src/main/java/net/hamza/banque/service/AuthenticationService.java
+++ src/main/java/net/hamza/banque/service/AuthenticationService.java
@@ -97,17 +97,17 @@ public class AuthenticationService {
             call.execute();
             OkHttpClient client = new OkHttpClient();
 
-            MediaType mediaType = MediaType.parse("application/json");
-            RequestBody bodysms = RequestBody.create(mediaType, "{\"type\":\"transactional\",\"unicodeEnabled\":false,\"sender\":\"pfs\",\"recipient\":\""+phone+"\",\"content\":\"your code to login : "+otp+"\"}");
-            Request requestsms = new Request.Builder()
-                    .url("https://api.brevo.com/v3/transactionalSMS/send")
-                    .post(bodysms)
-                    .addHeader("accept", "application/json")
-                    .addHeader("content-type", "application/json")
-                    .addHeader(env.getRequiredProperty("api.name"), env.getRequiredProperty("api.value"))
-                    .build();
-
-            Response responsesms = client.newCall(requestsms).execute();
+//            MediaType mediaType = MediaType.parse("application/json");
+//            RequestBody bodysms = RequestBody.create(mediaType, "{\"type\":\"transactional\",\"unicodeEnabled\":false,\"sender\":\"pfs\",\"recipient\":\""+phone+"\",\"content\":\"your code to login : "+otp+"\"}");
+//            Request requestsms = new Request.Builder()
+//                    .url("https://api.brevo.com/v3/transactionalSMS/send")
+//                    .post(bodysms)
+//                    .addHeader("accept", "application/json")
+//                    .addHeader("content-type", "application/json")
+//                    .addHeader(env.getRequiredProperty("api.name"), env.getRequiredProperty("api.value"))
+//                    .build();
+//
+//            Response responsesms = client.newCall(requestsms).execute();
             return otp;
 
         }
