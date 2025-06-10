9bed99c4054bff3325a76cde16dff6b15ac4d50e
fahlaoui <fahlaouimohammed@gmail.com>
2025-06-10 19:38:47 +0100

    ci/cd

--- src/main/java/net/hamza/banque/service/AuthenticationService.java
+++ src/main/java/net/hamza/banque/service/AuthenticationService.java
@@ -95,7 +95,7 @@ public class AuthenticationService {
                     " \"text\": \""+"your code to login is :  "+otp+"\"" +
                     "}";
             Request request = new Request.Builder()
-                    .url("http://127.0.0.1:3000/api/sendText")
+                    .url("http://13.51.109.196:3000/api/sendText")
                     .post(RequestBody.create(MediaType.parse("application/json"),body))
                     .build();
             Call call =new OkHttpClient().newCall(request);
